6c088c603e90ea411231e4038201aef854582f8c
Sam Cassidy <sam@cassidys.email>
2022-12-04 22:16:24 +0000

    Added my keybindings and modified the status line to show buffers in top bar

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,3 +1,28 @@
+local map = vim.keymap.set
+
+-- Normal Mode Remaps
+local normal = {
+  {"<C-d>", ":bd<cr>", {silent}}, -- delete current buffer
+  {"j", "v:count ? 'j' : 'gj'",{silent, expr=true}}, -- j goes down by visual lines, unless a count is provided
+  {"k", "v:count ? 'k' : 'gk'",{silent, expr=true}}, -- k goes up by visual lines, unless a count is provided
+}
+
+-- Insert mode remaps
+local insert = {
+  {"<A-l>", "<C-o>l", {silent}}, -- four mappings so that alt+hjkl moves cursor in insert
+  {"<A-k>", "<C-o>k", {silent}},
+  {"<A-j>", "<C-o>j", {silent}},
+  {"<A-h>", "<C-o>h", {silent}}
+}
+
+-- Set bindings
+for i, bind in ipairs(insert) do
+  map("i", unpack(bind))
+end
+
+for i, bind in ipairs(normal) do
+  map("n", unpack(bind))
+end
 
 local wk = require("which-key") 
 
@@ -7,6 +32,17 @@ wk.register({
   ["<leader>Ci"] = { "<cmd>e ~/.config/nvim/init.lua<CR>", "init.lua" },
   ["<leader>Cc"] = { "<cmd>e ~/.config/nvim/lua/colors.lua<CR>", "colours" },
   ["<leader>Cs"] = { "<cmd>e ~/.config/nvim/lua/settings.lua<CR>", "settings" },
+  ["<leader>Ck"] = { "<cmd>e ~/.config/nvim/lua/keybindings.lua<CR>", "settings" },
   ["<leader>Cp"] = { "<cmd>e ~/.config/nvim/lua/plugins.lua<CR>", "colours" },
   ["<leader>Cl"] = { "<cmd>e ~/.config/nvim/lua/statusline.lua<CR>", "statusline" },
+  ["<leader>CC"] = { "<cmd>so ~/.config/nvim/init.lua<CR>", "reload" },
+
+  ["<leader>P"] = { name = "+packer" },
+  ["<leader>Pi"] = { "<cmd>PackerInstall<CR>", "Install plugins"},
+  ["<leader>Ps"] = { "<cmd>PackerStatus<CR>", "View plugins status"},
+  ["<leader>Py"] = { "<cmd>PackerSync<CR>", "Sync plugins"},
+  ["<leader>Pu"] = { "<cmd>PackerUpdate<CR>", "Update plugins"}
+
 })
+
+
--- lua/statusline.lua
+++ lua/statusline.lua
@@ -4,10 +4,10 @@ require('lualine').setup({
   },
   tabline = {
       lualine_a = {'buffers'},
-      lualine_b = {'branch'},
+      lualine_b = {},
       lualine_c = {},
       lualine_x = {},
       lualine_y = {},
-      lualine_z = {}
+      lualine_z = {'branch'}
 }})
 --{options = {theme='auto'}}
