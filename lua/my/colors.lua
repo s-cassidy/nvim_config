8b2199500e2e35544dd7e5560e2f43c984fbef7f
Sam Cassidy <sam@cassidys.email>
2023-03-12 21:53:26 +0000

    colors move to separate file

--- lua/my/colors.lua
+++ lua/my/colors.lua
@@ -1,19 +1,7 @@
--- vim.o.background='dark'
-
 vim.api.nvim_create_autocmd("ColorScheme", { command = "highlight Normal ctermbg=NONE guibg=NONE" })
---require('ayu').setup({
---  overrides = {
---    panel_border = { fg = '#FFFFFF' }
---  }
---})
 
--- require('ayu').colorscheme()
-
-
-
--- require('nightfox').setup()
 vim.cmd('colorscheme kanagawa')
 vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal", bg = "NONE" })
 vim.api.nvim_set_hl(0, "SignColumn", { link = "Normal", bg = "NONE" })
 vim.api.nvim_set_hl(0, "FloatBorder", { fg = "fg" }) -- whatever your border color for the float is, check it with :highlight })
-vim.api.nvim_set_hl(0, "Comment", { fg = "#E82424" }) -- whatever your border color for the float is, check it with :highlight })
+vim.api.nvim_set_hl(0, "Comment", { fg = "#E82424" }) -- set comment color to red. comments should stand out not hide away!
