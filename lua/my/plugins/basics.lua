224e7bede13c96c33ba34f7136cba8d2aa5b7d71
Sam Cassidy <sam@cassidys.email>
2023-03-15 10:52:33 +0000

    move some "basic" plugins to separate file

---
+++ lua/my/plugins/basics.lua
@@ -0,0 +1,27 @@
+return {
+  -- Needed to load first
+  { 'lewis6991/impatient.nvim', priority = 100 },
+  { 'nathom/filetype.nvim', priority = 100 },
+  { 'nvim-lua/plenary.nvim' },
+  { 'nvim-lua/popup.nvim' },
+  { 'kyazdani42/nvim-web-devicons' },
+
+  { 'folke/which-key.nvim', lazy = true },
+
+  -- surround
+  { "kylechui/nvim-surround", event = "VeryLazy", config = function() require("nvim-surround").setup() end },
+
+  -- comments
+  { 'numToStr/Comment.nvim', event = "VeryLazy", config = function() require('Comment').setup() end },
+
+  -- tpope
+  { 'tpope/vim-repeat', event = "VeryLazy" }, -- Integrates the repeat
+  { 'tpope/vim-vinegar' }, -- Improves netrw
+
+  -- quality of life
+  { 'AckslD/nvim-neoclip.lua', config = function() require('neoclip').setup() end }, -- improves clipboard integration
+  { 'kevinhwang91/nvim-ufo', dependencies = 'kevinhwang91/promise-async' }, -- better folding
+  { 'tiagovla/scope.nvim', config = function() require('scope').setup() end, event = "VeryLazy" }, -- buffers are local to tabs
+
+  { 'drmingdrmer/vim-toggle-quickfix', lazy = true }, -- toggle the quickfix
+}
