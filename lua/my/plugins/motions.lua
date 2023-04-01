e024a68f3897844810dfdc59efaae5a45496ac05
Sam Cassidy <sam@cassidys.email>
2023-04-01 06:01:24 +0100

    add improved W, E, B motions

--- lua/my/plugins/motions.lua
+++ lua/my/plugins/motions.lua
@@ -4,4 +4,11 @@ return {
       require('leap').set_default_keymaps()
     end
   },
+  { 'chrisgrieser/nvim-spider', lazy = true, config = function()
+    vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+    vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-w" })
+    vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-w" })
+    vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-w" })
+  end
+  },
 }
