9d856a36f1171dc03499a66179abb5e816f56ff7
Sam Cassidy <sam@cassidys.email>
2023-05-04 20:18:55 +0100

    change snippet key

--- after/plugin/snippy.lua
+++ after/plugin/snippy.lua
@@ -1,7 +1,7 @@
 require('snippy').setup({
   mappings = {
     is = {
-      ['<C-f>'] = 'expand_or_advance',
+      ['<C-space>'] = 'expand_or_advance',
       ['<CR>'] = 'expand_or_advance',
       ['<C-x>'] = 'previous',
     },
