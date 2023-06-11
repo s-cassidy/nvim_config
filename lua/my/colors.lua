cecf1d788beac1be0e43d03572a6223cd4e7dce6
Sam Cassidy <sam@cassidys.email>
2023-06-11 11:44:25 +0100

    Remove colour on status line

--- lua/my/colors.lua
+++ lua/my/colors.lua
@@ -25,7 +25,7 @@ vim.cmd("colorscheme gruvbox")
 -- trasparency
 vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "NonText", { bg = "NONE" })
-
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal", bg = "NONE" })
 vim.api.nvim_set_hl(0, "SignColumn", { link = "Normal", bg = "NONE" })
 vim.api.nvim_set_hl(0, "FoldColumn", { link = "Normal", bg = "NONE" })
