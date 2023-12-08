ea22bbe9950bb7095b44c2842e9eabad8cf2e45e
Sam Cassidy <sam@cassidys.email>
2023-12-08 05:45:07 +0000

    Adjustment to harpoon binds

--- lua/my/plugin-config/harpoon.lua
+++ lua/my/plugin-config/harpoon.lua
@@ -10,15 +10,19 @@ local wk = require("which-key")
 
 --require("telescope").load_extension('harpoon')
 
-wk.register({
+local binds = {
   ["<leader>h"] = { name = "+harpoon" },
-  ["<leader>ha"] = { ':lua require("harpoon.mark").add_file()<CR>', "Add file to Harpoon" },
-  ["<leader>h]"] = { ':lua require("harpoon.ui").nav_next()<CR>', "Next file" },
-  ["<leader>h["] = { ':lua require("harpoon.ui").nav_prev()<CR>', "Prev file" },
-  ["<leader>hm"] = { ':lua require("harpoon.ui").toggle_quick_menu()<CR>', "Menu" },
-  ["<leader>ht"] = { ':Telescope harpoon marks<CR>', 'Telescope' },
-  ["<leader>fh"] = { ':Telescope harpoon marks<CR>', 'Harpoon' }
-})
+  { "<leader>ha", ':lua require("harpoon.mark").add_file()<CR>',        { desc = "Add file to Harpoon" } },
+  { "<leader>h]", ':lua require("harpoon.ui").nav_next()<CR>',          { desc = "Next file" } },
+  { "<leader>h[", ':lua require("harpoon.ui").nav_prev()<CR>',          { desc = "Prev file" } },
+  { "<leader>hm", ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { desc = "Menu" } },
+  { "<leader>ht", ':Telescope harpoon marks<CR>',                       { desc = 'Telescope' } },
+  { "<leader>fh", ':Telescope harpoon marks<CR>',                       { desc = 'Harpoon' } }
+}
+
+for _, bind in ipairs(binds) do
+  vim.keymap.set("n", unpack(bind))
+end
 
 vim.keymap.set("n", "<A-1>", ':lua require("harpoon.ui").nav_file(1)<CR>', { silent = true })
 vim.keymap.set("n", "<A-2>", ':lua require("harpoon.ui").nav_file(2)<CR>', { silent = true })
