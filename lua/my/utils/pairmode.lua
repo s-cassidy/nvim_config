95961282df4c2fc94d45d0dad2dbc04970a49c5b
Sam Cassidy <sam@cassidys.email>
2024-01-11 22:27:43 +0000

    Nicer pair mode

--- lua/my/utils/pairmode.lua
+++ lua/my/utils/pairmode.lua
@@ -1,5 +1,4 @@
-local start_pair_mode = function()
-  vim.cmd("set statuscolumn=%s%l\\ \\ %r")
-end
-
-vim.api.nvim_create_user_command("PairMode", start_pair_mode, {})
+vim.api.nvim_create_user_command("PairMode", function()
+    vim.cmd("set statuscolumn=%s%l\\ \\ %r")
+  end,
+  {})
