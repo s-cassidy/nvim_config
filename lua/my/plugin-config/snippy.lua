b4029d6c5241c38a7b520e356fd8e7f6e12c5d9a
Sam Cassidy <sam@cassidys.email>
2024-01-11 22:32:16 +0000

    Comment this out for now

--- lua/my/plugin-config/snippy.lua
+++ lua/my/plugin-config/snippy.lua
@@ -1,11 +1,11 @@
-require('snippy').setup({
-  mappings = {
-    is = {
-      ['<C-space>'] = 'expand_or_advance',
-      ['<C-k>'] = 'previous',
-    },
-    nx = {
-      ['<Tab>'] = 'cut_text',
-    },
-  },
-})
+-- require('snippy').setup({
+--   mappings = {
+--     is = {
+--       ['<C-space>'] = 'expand_or_advance',
+--       ['<C-k>'] = 'previous',
+--     },
+--     nx = {
+--       ['<Tab>'] = 'cut_text',
+--     },
+--   },
+-- })
