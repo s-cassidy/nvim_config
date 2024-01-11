410ce954b5360a88510c5eb2b0db28f9c5cad745
Sam Cassidy <sam@cassidys.email>
2024-01-11 22:29:14 +0000

    Set colors to kanagawa for now

--- lua/my/colors.lua
+++ lua/my/colors.lua
@@ -1,9 +1,4 @@
-vim.cmd[[colorscheme tokyonight-night]]
+-- vim.cmd [[colorscheme tokyonight-night]]
+vim.cmd.colorscheme "kanagawa"
 -- trasparency
-vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NonText", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal", bg = "NONE" })
-vim.api.nvim_set_hl(0, "SignColumn", { link = "Normal", bg = "NONE" })
-vim.api.nvim_set_hl(0, "FoldColumn", { link = "Normal", bg = "NONE" })
 vim.api.nvim_set_hl(0, "Comment", { fg = "#E82424" }) -- set comment color to red. comments should stand out not hide away!
