80a7b8054646b449d3418d339917cad92db5ab1a
Sam Cassidy <sam@cassidys.email>
2024-01-11 22:31:03 +0000

    Add react snippets

--- lua/my/plugin-config/luasnip.lua
+++ lua/my/plugin-config/luasnip.lua
@@ -1,4 +1,4 @@
---[[ require("luasnip.loaders.from_snipmate").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load()
 local ls = require("luasnip")
 local map = vim.keymap.set
 
@@ -7,8 +7,9 @@ ls.config.set_config({
   updateevents = "TextChanged,TextChangedI"
 })
 
-map({ "i", "s" }, "<C-j>", function()
-  if ls.expand_or_jumpable() then ls.expand_or_jump() end
-end, { silent = true })
 
-require("luasnip.loaders.from_lua").load({ paths = "./lua/my/snippets/" }) ]]
+require("luasnip.loaders.from_lua").load({ paths = "./lua/my/snippets/" })
+ls.filetype_extend("javascriptreact", { "javascript", "html" })
+-- vim.keymap.set({ "i", "s" }, "<C-spc>", function()
+--   if ls.expand_or_jumpable() then ls.expand_or_jump() end
+-- end, { silent = true })
