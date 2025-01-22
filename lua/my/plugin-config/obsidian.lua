12066aaf271c45b5774804323be2cc3679a1cf00
Sam Cassidy <sam@cassidys.email>
2025-01-22 06:16:01 +0000

    Add note extract function

--- lua/my/plugin-config/obsidian.lua
+++ lua/my/plugin-config/obsidian.lua
@@ -1,4 +1,8 @@
-local vault_path = "~/notes/"
+local actions = require("telescope.actions")
+local finders = require("telescope.finders")
+local telescope_conf = require("telescope.config").values
+local action_state = require "telescope.actions.state"
+local vault_path = vim.fn.expand("~/notes/")
 
 require("obsidian").setup({
   workspaces = {
@@ -49,6 +53,40 @@ vim.keymap.set(
 ]]
 --
 
+
+local function get_text()
+  local content_lines
+  local visual_start
+  local visual_end
+  if vim.fn.mode() == "n" then
+    content_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  elseif vim.fn.mode() == "v" or vim.fn.mode() == "V" then
+    visual_start = vim.fn.getpos("v")
+    visual_end = vim.fn.getpos(".")
+    -- "." is the cursor position, "v" is the other end of the visual selection.
+    -- They must be swapped if the cursor is at the start of the visual selection
+    if visual_start[2] > visual_end[2] or (
+          visual_start[2] == visual_end[2] and visual_start[3] > visual_end[3]
+        ) then
+      visual_start, visual_end = visual_end, visual_start
+    end
+    if vim.fn.mode() == "v" then
+      content_lines = vim.api.nvim_buf_get_text(
+        0,
+        visual_start[2] - 1,
+        visual_start[3] - 1,
+        visual_end[2] - 1,
+        visual_end[3],
+        {}
+      )
+    else
+      content_lines = vim.api.nvim_buf_get_lines(0, visual_start[2] - 1, visual_end[2], false)
+    end
+  end
+  return content_lines, visual_start[2] - 1, visual_end[2]
+end
+
+
 local clear_and_add_template = function(template)
   vim.cmd("e! %")
   vim.cmd("execute 'normal dd'")
@@ -56,27 +94,65 @@ local clear_and_add_template = function(template)
   vim.cmd("call feedkeys(':','nx')") -- clear message window
 end
 
-local new_note = function(folder)
+local create_note = function(filename, folder, text)
+  local buffer = vim.api.nvim_create_buf(true, false)
+  vim.api.nvim_set_current_buf(buffer)
+  vim.bo.filetype = "markdown"
+  vim.cmd("w " .. vault_path .. folder .. "/" .. filename .. ".md")
+  vim.cmd("ObsidianTemplate note")
+  if text then
+    vim.api.nvim_buf_set_lines(buffer, 7, 7, false, text)
+  end
+  vim.cmd("w")
+  vim.cmd("Git add .")
+  vim.cmd("Git commit -q -m 'Create note " .. tostring(os.date("%Y%m%d%H%M")) .. "'")
+end
+
+local new_note = function(title, text)
+  local filename
+  if not title then
+    vim.ui.input({ prompt = "Note title: " },
+      function(input)
+        title = input
+      end)
+  end
+  filename = tostring(os.date("%Y%m%d%H%M")) .. " " .. title
+  require("telescope.pickers").new(opts, {
+    prompt_title = "Move to...",
+    finder = finders.new_oneshot_job({ "fdfind", "-td" }, {}),
+    sorter = telescope_conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        local selection = action_state.get_selected_entry()
+        if selection ~= nil then
+          actions.close(prompt_bufnr)
+          create_note(filename, selection[1], text)
+        end
+      end)
+      return true
+    end,
+  }):find()
+end
+
+local extract_note = function()
+  local text
+  local start_line
+  local end_line
+  text, start_line, end_line = get_text()
   local title
   vim.ui.input({ prompt = "Note title: " },
     function(input)
       title = input
     end)
   local filename = tostring(os.date("%Y%m%d%H%M")) .. " " .. title
-  local buffer = vim.api.nvim_create_buf(true, false)
-  vim.api.nvim_set_current_buf(buffer)
-  vim.bo.filetype = "markdown"
-  vim.cmd("w " .. vault_path .. filename .. ".md")
-  vim.cmd("ObsidianTemplate note")
-  vim.cmd("w")
-  vim.cmd("Git add .")
-  vim.cmd("Git commit -q -m 'Create note " .. tostring(os.date("%Y%m%d%H%M")) .. "'")
+  vim.api.nvim_buf_set_lines(0, start_line, end_line, true, { ("Extracted note to [[%s]]"):format(filename) })
+  new_note(title, text)
 end
 
 local daily_note = function()
   local filename = tostring(os.date("%Y-%m-%d-%a"))
-  local f = io.open(vault_path .. "journal/" .. filename .. ".md")
-  if f ~= nil then
+  local f = io.open(vault_path .. "journal/" .. filename .. ".md", "r")
+  if f then
     io.close(f)
     vim.cmd("e" .. vault_path .. "journal/" .. filename .. ".md")
   else
@@ -84,9 +160,9 @@ local daily_note = function()
     vim.api.nvim_set_current_buf(buffer)
     vim.bo.filetype = "markdown"
     vim.cmd("w " .. vault_path .. "journal/" .. filename .. ".md")
-    vim.cmd("ObsidianTemplate daily")
+    vim.cmd("ObsidianTemplate daily-nvim")
     local title_line = "# " .. tostring(os.date("%Y-%m-%d | %A"))
-    vim.api.nvim_buf_set_lines(buffer, 5, 6, true, { title_line })
+    vim.api.nvim_buf_set_lines(buffer, 5, 5, true, { title_line })
     vim.cmd("w")
     vim.cmd("Git add .")
     vim.cmd("Git commit -q -m 'Create note " .. tostring(os.date("%Y-%m-%d-%a")) .. "'")
@@ -115,12 +191,8 @@ local find_in_folder = function()
 end
 
 local move_to_folder = function()
-  local actions = require("telescope.actions")
-  local finders = require("telescope.finders")
-  local telescope_conf = require("telescope.config").values
-  local action_state = require "telescope.actions.state"
   require("telescope.pickers").new(opts, {
-    prompt_title = "Find Directory",
+    prompt_title = "Move to...",
     finder = finders.new_oneshot_job({ "fdfind", "-td" }, {}),
     sorter = telescope_conf.generic_sorter(opts),
     attach_mappings = function(prompt_bufnr, map)
@@ -136,6 +208,7 @@ local move_to_folder = function()
   }):find()
 end
 
+vim.keymap.set("v", "<leader>vx", extract_note, { desc = "Extract note" })
 
 require("which-key").add({
   { "<leader>v",   name = "+obsidian" },
