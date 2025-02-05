968ac273d1481785eac9bbeedcfe0ececf310fc3
Sam Cassidy <sam@cassidys.email>
2025-02-05 07:01:39 +0000

    Change colors

--- lua/my/colors.lua
+++ lua/my/colors.lua
@@ -1,3 +1,3 @@
-vim.cmd [[colorscheme darkforest]]
+vim.cmd [[colorscheme evergarden]]
 -- vim.cmd.colorscheme ""
 -- vim.api.nvim_set_hl(0, "Comment", { fg = "#E82424" }) -- set comment color to red. comments should stand out not hide away!
