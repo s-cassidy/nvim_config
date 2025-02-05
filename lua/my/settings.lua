9045ee7efb18a2dbe36916c09e5f7abeb712b73c
Sam Cassidy <sam@cassidys.email>
2025-02-05 07:01:35 +0000

    Nunjucks = html

--- lua/my/settings.lua
+++ lua/my/settings.lua
@@ -69,3 +69,11 @@ augroup highlight_yank
     autocmd!
     au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=100 }
 augroup END]])
+
+-- filetypes
+
+vim.filetype.add({
+  extension = {
+    njk = "html",
+  },
+})
