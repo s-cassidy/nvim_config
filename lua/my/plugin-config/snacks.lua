baef8a76f14fa71b5eaefa355614c51ef862b5f1
Sam Cassidy <sam@cassidys.email>
2025-02-13 06:01:50 +0000

    Enable snacks.input

--- lua/my/plugin-config/snacks.lua
+++ lua/my/plugin-config/snacks.lua
@@ -3,7 +3,7 @@ return {
   -- or leave it empty to use the default settings
   -- refer to the configuration section below
   bigfile = { enabled = true },
-  input = { enabled = true },
+  -- input = { enabled = true },
   picker = { enabled = true },
   notifier = { enabled = true },
   quickfile = { enabled = true },
