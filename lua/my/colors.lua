a402c3c5020ebb46a650829c44980c29641ab4f0
sam <sam@cassidys.email>
2025-06-21 15:19:25 +0100

    Add light/dark toggle

--- lua/my/colors.lua
+++ lua/my/colors.lua
@@ -1,3 +1,18 @@
+local light_colour = "kanso-pearl"
+local dark_colour = "kanso-zen"
+local colour_mode = "dark"
 vim.cmd [[colorscheme kanso]]
+
+local function toggle_light_dark()
+ if colour_mode == "dark" then
+    colour_mode = "light"
+    vim.cmd("colorscheme " .. light_colour)
+  else
+    colour_mode = "dark"
+    vim.cmd( "colorscheme " .. dark_colour)
+  end
+end
+
+vim.keymap.set("n", "<F2>", toggle_light_dark, {desc = "Toggle light/dark mode"})
 -- vim.cmd.colorscheme ""
 -- vim.api.nvim_set_hl(0, "Comment", { fg = "#E82424" }) -- set comment color to red. comments should stand out not hide away!
